b4fe663999ae7678f1abb27de598ea2a5e9b02be
Pewdoloco <ersovlevd@gmail.com>
2025-05-28 22:57:31 +0300

    update readme, style.css, index.html

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.getElementById("rss-from").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const rssUrl = document.getElementById("rss-url").ariaValueMax;
+  new Promise((resolve, reject) => {
+    if (rssUrl) {
+      resolve(`RSS URL received: ${rssUrl}`);
+    } else {
+      reject(new Error("URL is empty"));
+    }
+  })
+    .then((message) => {
+      console.log(message);
+      
+      
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+});
